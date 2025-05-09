53f0d02a719d4df2473be059c436f05bff0ddb68
noyeonu <zkxmzkxm1983@naver.com>
2025-05-09 15:55:59 +0900

    fix 맵, 함정, 캐릭터
    캐릭터 움직임 코드 작성

--- Assets/Scripts/trap.cs
+++ Assets/Scripts/trap.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class Trap2D : MonoBehaviour
 {
     // 플레이어(테스트용 캐릭터)가 트리거에 들어왔을 때
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D Trap)
     {
         // Tag를 Player로 해뒀다면
-        if (other.CompareTag("Player"))
+        if (Trap.CompareTag("Player"))
         {
             Debug.Log("죽었습니다");
         }
